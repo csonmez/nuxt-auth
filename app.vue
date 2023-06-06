f6c67af578ee1676e519a6cc3dd76e0f371dbdc0
cemsonmez <cem@gdlabs.io>
2023-06-06 15:56:25 +0300

    remove unused function and edit function names

--- app.vue
+++ app.vue
@@ -6,40 +6,28 @@ await useAsyncData('users', () => getMe())
 
 const user = useUser()
 
-const register2 = async () => {
+const handleRegister = async () => {
     try {
         await register({ name: 'cem', surname: 'sönmez', email: 'cem@cem.com', password: 'Cem123..' })
-        console.log('register')
     } catch (err) {
         console.log('err', err)
     }
 }
 
-const login2 = async () => {
+const handleLogin = async () => {
     try {
         await login({ email: 'cem@cem.com', password: 'Cem123..' })
-        console.log('login')
         await getMe()
-        console.log('user2', user)
-        // user = authStore.user
-        // users = store.user.value
-        // console.log('authStore.user', authStore.user.email)
-        // console.log('store.user', store.user.value.email)
     } catch (err) {
         console.log('err', err)
     }
-}
-
-const update = () => {
-    user.email.value = 'deneme@denene.com'
 }
 </script>
 
 <template>
     <div>
-        <button @click="register2">Register</button>
-        <button @click="login2">Login</button>
+        <button @click="handleRegister">Register</button>
+        <button @click="handleLogin">Login</button>
         <span>{{ user && user.email }}</span>
-        <button @click="update">Update email</button>
     </div>
 </template>
